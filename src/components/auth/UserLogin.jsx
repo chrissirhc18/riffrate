71554ab7e164ea594463db0464bbaf52a8a2c79b
Marina Hishikawa <marinahishikawa@Marinas-MacBook-Pro.local>
2025-11-19 21:29:04 -0600

    login/register pages updated

--- src/components/auth/UserLogin.jsx
+++ src/components/auth/UserLogin.jsx
@@ -1,3 +1,6 @@
+import { Form, Button } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router';
+
 /* TODO
 - username input
 - password input
@@ -6,8 +9,33 @@
 - login button on nav bar should change to a logout button
 */
 
+
 export default function UserLogin (props) {
+
+    const username = ""; //FIXME
+    const password = ""; //FIXME
+
+    const navigate = useNavigate();
+
+    function handleLogIn(e) {
+        /* TODO */
+        navigate("/");
+    }
+
     return <div>
-        <h1>User should log in here</h1>
+        <h1>Login Below</h1>
+        <Form onSubmit={handleLogIn}>
+            <Form.Label htmlFor="usernameInput">Username:</Form.Label>
+            <Form.Control id="usernameInput" />
+            <Form.Label htmlFor="passwordInput">Password:</Form.Label>
+            <Form.Control id="passwordInput" type="password" />
+            <br />
+            <Button type="submit">Login</Button>
+        </Form>
+        <br />
+        <br />
+        <Link to="/UserRegister">
+                <button type="button" className="btn btn-outline-dark">Don't have an account?</button>
+            </Link>
     </div>
 }
--- src/components/auth/UserRegister.jsx
+++ src/components/auth/UserRegister.jsx
@@ -1,3 +1,6 @@
+import { Form, Button } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router';
+
 /* TODO
 - username input
 - password input
@@ -7,7 +10,30 @@
 */
 
 export default function UserRegister (props) {
+
+    const navigate = useNavigate();
+
+    function handleLogIn(e) {
+        /* TODO */
+        navigate("/");
+    }
+
     return <div>
-        <h1>User should register here</h1>
+        <h1>Sign up Below</h1>
+        <Form onSubmit={handleLogIn}>
+            <Form.Label htmlFor="usernameInput">Username:</Form.Label>
+            <Form.Control id="usernameInput" />
+            <Form.Label htmlFor="passwordInput">Password:</Form.Label>
+            <Form.Control id="passwordInput" type="password" />
+            <Form.Label htmlFor="confPasswordInput">Confirm Password:</Form.Label>
+            <Form.Control id="confPasswordInput" type="password" />
+            <br />
+            <Button type="submit">Login</Button>
+        </Form>
+        <br />
+        <br />
+        <Link to="/UserLogin">
+                <button type="button" className="btn btn-outline-dark">Have an account?</button>
+            </Link>
     </div>
 }
